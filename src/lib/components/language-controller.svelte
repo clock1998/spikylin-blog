10290ca10185a80ed12806f06f06f065177853bb
linyi <jianglinyi97@gmail.com>
2025-03-14 00:17:51 -0400

    fix language selector

--- src/lib/components/language-controller.svelte
+++ src/lib/components/language-controller.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { availableLanguageTags, languageTag, type AvailableLanguageTag } from '$lib/paraglide/runtime';
 	import { i18n } from '$lib/i18n';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import { goto } from '$app/navigation';
 
 	function switchToLanguage(newLanguage: AvailableLanguageTag) {
-		const canonicalPath = i18n.route($page.url.pathname);
+		const canonicalPath = i18n.route(page.url.pathname);
 		const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
 		goto(localisedPath);
 	}
@@ -15,7 +15,7 @@
     }
 </script>
 
-<select class="select select-sm ml-2" on:change={e => switchToLanguage(e.currentTarget.value as AvailableLanguageTag)}>
+<select class="select select-sm ml-2 w-20" on:change={e => switchToLanguage(e.currentTarget.value as AvailableLanguageTag)}>
     {#each availableLanguageTags as langTag}
         <option 
             value={langTag}
